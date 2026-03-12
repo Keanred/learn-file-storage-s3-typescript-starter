a987708a96801307bca7a5199cf5860ef030ed8f
Keanred <47241653+Keanred@users.noreply.github.com>
2026-03-12 12:08:18 +0200

    add thumnails and encoding

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { BadRequestError, UserForbiddenError } from "./errors";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -47,7 +16,26 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData()
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Invalid thumbnail file");
+  }
+  const maxUploadSize = 2 << 20;
+  if (file.size > maxUploadSize) {
+    throw new BadRequestError("Thumbnail file is too large (max 10MB)");
+  }
+  const fileType = file.type;
+  const fileBuffer = Buffer.from(await file.arrayBuffer());
+  const stringBuffer = fileBuffer.toString("base64");
+  const dataUrl = `data:${fileType};base64,${stringBuffer}`;
+  const video = getVideo(cfg.db, videoId);
+  if (userID !== video?.userID) {
+    throw new UserForbiddenError("You don't have permission to upload a thumbnail for this video");
+  }
 
-  return respondWithJSON(200, null);
+  video.thumbnailURL = dataUrl;
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
